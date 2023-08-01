27578c54331f361711c03eadf28ddf433e171981
Konstantin Kochetkov <ka4ah4uk@yandex.ru>
2023-08-01 10:39:20 +0300

    Initial commit

---
+++ Profile | Surf Summer School 2023/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Profile | Surf Summer School 2023
+//
+//  Created by Ka4aH on 01.08.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
